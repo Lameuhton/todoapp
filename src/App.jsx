55fb28e289b2fdd67e15f1761ccc0a44ce3e0f92
Lameuhton <aurelianeg2000@gmail.com>
2024-04-22 22:31:12 +0200

    finished (added modify and delete todos)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editingText, setEditingText] = useState('');
 
   //Charge depuis le local storage
   useEffect(() => {
@@ -43,12 +45,33 @@ function App() {
     );
   };
 
+  const handleDelete = (index) => {
+    setTodos(prevTodos =>
+      prevTodos.filter((_, i) => i !== index)
+    );
+  };
+
+  const handleEdit = (index) => {
+    setEditingIndex(index);
+    setEditingText(todos[index].text);
+  };
+
+  const handleSaveEdit = () => {
+    setTodos(prevTodos =>
+      prevTodos.map((todo, i) =>
+        i === editingIndex ? { ...todo, text: editingText } : todo
+      )
+    );
+    setEditingIndex(null);
+    setEditingText('');
+  };
+
   return (
     <>
       <h1>My Todo App</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="newTodo">Type a new todo:</label>
-        <input type="text" id="newTodo" name="newTodo" value={newTodo} onChange={handleInputChange}/>
+        <input type="text" id="newTodo" name="newTodo" value={newTodo} onChange={handleInputChange} required/>
         <button type="submit">Add Todo</button>
       </form>
       <hr/>
@@ -56,13 +79,26 @@ function App() {
         <h2>Todos:</h2>
         <ul>
           {todos.map((todo, index) => (
-            <li key={index}>
-              <label>
-                <input type="checkbox" checked={todo.completed} onChange={() => handleToggle(index)}/>
-                {todo.text}
-              </label>
-            </li>
-          ))}
+              <li key={index}>
+                {editingIndex === index ? (
+                  <>
+                    <input type="text" value={editingText} onChange={(e) => setEditingText(e.target.value)}/>
+                    <button onClick={handleSaveEdit}>Save</button>
+                  </>
+                ) : (
+                  <>
+                    <label>
+                      <input type="checkbox" checked={todo.completed} onChange={() => handleToggle(index)}/>
+                      {todo.text}
+                    </label>
+                    <div>
+                      <button onClick={() => handleEdit(index)} className="pen">✏️</button>
+                      <button onClick={() => handleDelete(index)} className="trash">🗑️</button>
+                    </div>
+                  </>
+                )}
+              </li>
+            ))}
         </ul>
       </section>  
     </>
