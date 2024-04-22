c4ac50c6161a44b2ffab3e433def1eba96f75c56
Lameuhton <aurelianeg2000@gmail.com>
2024-04-22 16:03:13 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +6,19 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+
+  //Charge depuis le local storage
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  //Change les todos dans le local storage à chaque changement
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const handleInputChange = (event) => {
     setNewTodo(event.target.value);
@@ -16,10 +29,18 @@ function App() {
     if (newTodo.trim() !== '') {
       setTodos(prevTodos => [
         ...prevTodos,
-        newTodo
+        { text: newTodo, completed: false }
       ]);
       setNewTodo('');
     };
+  };
+
+  const handleToggle = (index) => {
+    setTodos(prevTodos =>
+      prevTodos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
   };
 
   return (
@@ -35,16 +56,15 @@ function App() {
         <h2>Todos:</h2>
         <ul>
           {todos.map((todo, index) => (
-              <li key={index}>
-            <label>
-              <input type="checkbox"/>
-              {todo}
-            </label>
-          </li>
-            ))}
+            <li key={index}>
+              <label>
+                <input type="checkbox" checked={todo.completed} onChange={() => handleToggle(index)}/>
+                {todo.text}
+              </label>
+            </li>
+          ))}
         </ul>
-      </section>
-      
+      </section>  
     </>
   )
 };
